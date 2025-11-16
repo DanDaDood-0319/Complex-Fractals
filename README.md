c55fc1cd3684662833c8fde52c970670c742b1ea
DanDaDood <toffee.m.devera@gmail.com>
2025-11-16 15:02:44 +0800

    Optimization and rewrite
    Tried to optimize complex_fractals.py. Also, internet is down, so please remind myself to add the links later.

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # ****Complex Fractals****
-This is [DanDaDood-0319](https://github.com/DanDaDood-0319)'s fractal renderer. As you see here, currently, it sucks. This is my second project with [PyGame](https://www.pygame.org/), so please don't cringe.
+This is [DanDaDood-0319](https://github.com/DanDaDood-0319)'s [complex fractal]() renderer.
+
+## Dependencies
+This requires [Python](http://python.org/) *(duh)* as well as the following libraries:
+* **math** for... well, math! *(comes with a generic installation of [Python](http://python.org/))*installation of [Python](http://python.org/))*
+* **[colorsys]()** for converting HSV into RGB for the coloring.
+* **[pygame](https://www.pygame.org/)** for rendering.
+
+## Personal Notes
+* This is my second project with **[PyGame](https://www.pygame.org/)** and **[TkInter]()**! *(TkInter not implemented yet)*
+  * *(also my first README.md file!)* 
+* Expect it to suck, because I'm not that good at programming This is just. Also, as you have noticed, this is *very*, ***very*** informal.
+* I don't expect anyone to see this, but if you do, then **hi**!
 
 ## License
-[GNU General Public License, Version 3.0]()
-```
-This is a complex fractal renderer. It renders complex fractals, such as the mandelbrot set.
-  Copyright (C) 2025  DanDaDood-0319
+**[GNU General Public License, Version 3.0](https://www.gnu.org/licenses/)**
+
+This is a [complex fractal]() renderer. It renders complex fractals, such as the mandelbrot set.
+  Copyright (C) 2025  [DanDaDood-0319](https://github.com/DanDaDood-0319)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,7 +31,3 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
-```
-
-## NOTE TO SELF:
-**PLEASE CHANGE THIS WHEN IT DOESN'T SUCK. ALSO, THIS IS A PERSONAL PROJECT**
--- complex_fractals.py
+++ complex_fractals.py
@@ -1,39 +1,45 @@
 import pygame as pg
-import random as rand
 import math as m
-import cmath as cm
 import colorsys as csys
 zoom,zoom_r=1,14
 pg.display.set_caption("Toffee's Mandelbrot Set Explorer")
 s_w,s_h = 480,360
 s = pg.display.set_mode((s_w,s_h))
-c_x,c_y=-0.8,0
-m_vx,m_vy=0,0
-res=5
-sqrt2=m.sqrt(2)
+c_x, c_y = -0.8, 0
+m_vx,m_vy = 0,0
+res = 5
+sqrt2, log10_2 = m.sqrt(2), m.log10(2)
 run = True
 def dist(x,y):
   return((x*x+y*y)**0.5)
 
 def mandelbrot(z_r,z_i,p_r,p_i,c_r,c_i,r):
+  z_rn, z_in = z_r, z_i
   for i in range(r):
-    z_r,z_i=((z_r*z_r)-(z_i*z_i))+c_r,(2*z_r*z_i)+c_i
-    if dist(z_r,z_i)>=2:
-      return(((i+1)-(m.log10(m.log10(dist(z_r,z_i)))/m.log10(2)))%100)
+    z_rn,z_in=(z_rn*z_rn)-(z_in*z_in)+c_r,(2*z_rn*z_in)+c_i
+    if dist(z_rn,z_in)>=2:
+      return(((i+1)-(m.log10(m.log10(dist(z_rn,z_in)))/log10_2)))
   return(-1)
 
+def if(c)then(t)else(f)
+  if(c)
+    return(t)
+  
+
 while run:
-  x,y=0,0
-  res_t=res/2
-  s_wr=m.ceil(s_w/res)
+  x,y = 0,0
+  res_2 = res/2
+  s_wr = m.ceil(s_w/res)
+  s_w2, s_h2 = s_w/2, s_h/2
   for i in range(m.ceil(s_h/res)):
     scroll=0
     for i_2 in range(s_wr):
-      c_o = mandelbrot(0,0,2,0,(((x+res_t)-(s_w/2))/zoom)+c_x,(((y+res_t)-(s_h/2))/zoom)+c_y,500)
+      c_o = mandelbrot(0,0,2,0,(((x + res_2) - s_w2)/zoom) + c_x,(((y + res_2) - s_h2) / zoom) + c_y,500)
       if c_o == -1:
         c = (0,0,0)
       else:
-        c = tuple(255*x for x in csys.hsv_to_rgb(c_o/100,1,1))
+        c = tuple((x * 256)%256 for x in csys.hsv_to_rgb(c_o*0.015625,1,1))
+      print(c)
       pg.Surface.fill(s,c,(x,y,res,res))
       for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -45,8 +51,8 @@ while run:
           else:
             scroll=-1
       x+=res
-    pg.draw.circle(s,(0,0,0),(s_w/2,s_h/2),4)
-    pg.draw.circle(s,(255,255,255),(s_w/2,s_h/2),3)
+    pg.draw.circle(s,(0,0,0),(s_w2,s_h2),4)
+    pg.draw.circle(s,(255,255,255),(s_w2,s_h2),3)
     pg.display.update()
     if (pg.mouse.get_pressed())[0]:
       if m_sx == "a":
